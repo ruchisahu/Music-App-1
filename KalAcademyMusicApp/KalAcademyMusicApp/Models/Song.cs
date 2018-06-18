43fdacaa6ef82e7dc47315d856410640e10a7e5f
tigeringarden <steventranucsd@outlook.com>
2018-06-18 13:12:53 -0700

    add Song & User models

---
+++ KalAcademyMusicApp/KalAcademyMusicApp/Models/Song.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalAcademyMusicApp.Models
+{
+    public class Song
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Artist { get; private set; }
+
+        public Song(int id, string name, string artist)
+        {
+            Id = id;
+            Name = name;
+            Artist = artist;
+        }
+    }
+}
---
+++ KalAcademyMusicApp/KalAcademyMusicApp/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalAcademyMusicApp.Models
+{
+    public class User
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Password { get; private set; }
+        public List<Song> OwnedSongs { get; set; }
+        public List<Song> FavoriteSongs { get; set; }
+
+        public User(int id, string name, string password)
+        {
+            Id = id;
+            Name = name;
+            Password = password;
+        }
+    }
+}
