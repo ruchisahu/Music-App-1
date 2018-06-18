3691584c8cfc21ac19ad9209109c7ed489029206
tigeringarden <steventranucsd@outlook.com>
2018-06-18 13:28:46 -0700

    add Album model

---
+++ KalAcademyMusicApp/KalAcademyMusicApp/Models/Album.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace KalAcademyMusicApp.Models
+{
+    class Album
+    {
+        public Image Image { get; private set; }
+        public string Name { get; private set; }
+        public List<Song> CollectedSongs { get; private set; }
+
+        public Album(Image image, string name)
+        {
+            Image = image;
+            Name = name;
+        }
+    }
+}
