9f10fb886558e4715f17bb826581de546ab87f3d
tigeringarden <steventranucsd@outlook.com>
2018-07-05 22:31:00 -0700

    hide application title bar for visual effect

--- KalAcademyMusicApp/KalAcademyMusicApp/App.xaml.cs
+++ KalAcademyMusicApp/KalAcademyMusicApp/App.xaml.cs
@@ -1,18 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Windows.ApplicationModel.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 namespace KalAcademyMusicApp
@@ -71,6 +62,10 @@ namespace KalAcademyMusicApp
                 // Ensure the current window is active
                 Window.Current.Activate();
             }
+
+            // these settings modify the visual appearance of the application bar
+            var coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            coreTitleBar.ExtendViewIntoTitleBar = true;
         }
 
         /// <summary>
